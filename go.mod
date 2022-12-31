3de270286831fdb426f88aa6e3c94dfcb8939aca
yeremiamarcellius <66683514+yeremiamarcellius@users.noreply.github.com>
2022-12-31 14:33:08 +0700

    echo

---
+++ go.mod
@@ -0,0 +1,5 @@
+module bncc
+
+go 1.15
+
+require github.com/labstack/echo/v4 v4.9.1
---
+++ server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+ "net/http"
+ 
+ "github.com/labstack/echo/v4"
+)
+
+// func main() {
+//  e := echo.New()
+
+//  e.GET("/", func(c echo.Context) error {
+//   return c.String(http.StatusOK, "Hello, World!")
+//  })
+//  e.Logger.Fatal(e.Start(":1323"))
+
+//  e.GET("/users/:id", getUser)
+//  // e.POST("/users", saveUser)
+//  // e.PUT("/users/:id", updateUser)
+//  // e.DELETE("/users/:id", deleteUser)
+// }
+
+// func getUser(c echo.Context) error {
+//  // User ID from path users/:id
+//  id := c.Param("id")
+//  return c.String(http.StatusOK, id)
+// }
+
+func getUser(c echo.Context) error {
+ // User ID from path users/:id
+ id := c.Param("id")
+ return c.String(http.StatusOK, id)
+}
+
+func welcoming(c echo.Context) error {
+ return c.String(http.StatusOK, "Hello, World!")
+}
+
+func main() {
+ e := echo.New()
+
+ e.GET("/", welcoming)
+ e.GET("/users/:id", getUser)
+
+ // e.POST("/users", saveUser)
+ // e.PUT("/users/:id", updateUser)
+ // e.DELETE("/users/:id", deleteUser)
+ e.Logger.Fatal(e.Start(":1323"))
+}
